089541b2e443e8ccb191a411958d39ecee60d884
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-09-30 17:10:05 +0000

    page generator now generates pages
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa_global/trunk/framework@321494 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/generators/page_generator.rb
+++ lib/generators/page_generator.rb
@@ -2,20 +2,18 @@ require 'need'
 need { 'base_generator' }
 
 class PageGenerator < BaseGenerator
-#  attr_accessor :file_name
-#  
-#  def initialize(file_name)
-#    @file_name = file_name
-#  end
-#  
-#  def folder_path
-#    File.join('lib','sites',@site_name)
-#  end
-#  
-#  def generate
-#    file "page.rb.erb", File.join(folder_path,"#{file_name}.rb")
-#    folder File.join(folder_path,"#{file_name}")
-#    folder File.join(folder_path,"#{file_name}","flows")
-#    folder File.join(folder_path,"#{file_name}","pages")
-#  end
+  attr_accessor :file_name
+  def initialize(file_name,site_name)
+    @file_name = file_name
+    @site_name = site_name
+  end
+  
+  def folder_path
+    File.join('lib','sites')
+  end
+  
+  def generate
+    file "page.rb.erb", File.join(folder_path, @site_name, "pages",  "#{@file_name}.rb")
+  end 
+   
 end
--- spec/page_generator_spec.rb
+++ spec/page_generator_spec.rb
@@ -5,11 +5,43 @@ need { 'spec_helper' }
 need { '../lib/generators/page_generator' }
 
 describe PageGenerator do
-
-  it "should have a template file in the templates directory" do
-    File.exists?('lib/generators/templates/page.rb.erb').should be_true
+  
+  before :all do
+    @site_name = "Gap"
+    @site_folder = File.join("spec","gap")
+    @site_file = File.join("spec","gap.rb")
+    @page_name = "CheckOut"
+    @page_file = File.join("spec", "gap", "pages" , "check_out.rb")
+  end
+  
+  before :each do
+    SiteGenerator.any_instance.stubs(:folder_path).returns(File.join("spec"))
+    SiteGenerator.new(@site_name).generate
+  end
+    
+  after :each do
+    FileUtils.rm_rf(@site_folder)
+    FileUtils.rm_rf(@site_file)
   end
 
-  it "should generate a page file in lib/\#{site_name}/pages/"
-  it "should generate a file that can be required"
+  it "should generate a page file in lib/\#{site_name}/pages/" do
+    PageGenerator.any_instance.stubs(:folder_path).returns(File.join("spec"))
+    generator = PageGenerator.new(@page_name,@site_name)
+    generator.generate
+    File.exists?(@page_file).should be_true
+  end
+  
+  it "should generate a file that can be required" do
+    PageGenerator.any_instance.stubs(:folder_path).returns(File.join("spec"))
+    generator = PageGenerator.new(@page_name,@site_name)
+    generator.generate
+    system("ruby -c #{@page_file} > /dev/null").should be_true
+  end
+  
+  
+  it " should have the right folder path generated " do
+    
+    generator = PageGenerator.new("","")
+    generator.folder_path.should eql("lib/sites")
+  end
 end
