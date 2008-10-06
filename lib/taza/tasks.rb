2a45ad8ad78364c8d9b9746ea2e066a5638fd7b1
adanderson <adanderson@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-10-06 18:01:09 +0000

    Adding descriptions for rake tasks
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa_global/trunk/framework@322173 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/taza/tasks.rb
+++ lib/taza/tasks.rb
@@ -21,20 +21,24 @@ end
 
 public
 
+desc "invoke tests by tag"
 Rake::TestTask.new :test_tag do |t|
   t.test_files = get_tagged_files('test/**/test_*.rb')
 end
 
+desc "invoke specs by tag"
 Spec::Rake::SpecTask.new :spec_tag do |t|
   t.spec_files = get_tagged_files('spec/**/*_spec.rb')
 end
 
 namespace :generate do
+  desc "generate a site(rake generate:site name=foo)"
   task :site do
     validate_required_environment_input_present("Usage: rake generate:site name=the_site_name",'name')
     Taza::Generators::Site.new(ENV['name']).generate
   end
 
+  desc "generate a page(rake generate:page site=foo page=bar)"
   task :page do
     validate_required_environment_input_present("Usage: rake generate:page name=the_page_name site=the_site_name",'name','site')
     Taza::Generators::Page.new(ENV['name'],ENV['site']).generate
