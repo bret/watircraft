9e7a2e172415dc6e9e8d514a7f795c8b588e5f34
adanderson <adanderson@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-10-06 18:01:15 +0000

    removing invalid spec about windows bat file
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa_global/trunk/framework@322174 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/taza_bin_spec.rb
+++ spec/taza_bin_spec.rb
@@ -15,8 +15,6 @@ describe "Taza project generator script" do
     system("#{taza_bin}").should be_true
   end
 
-  it "should have a windows version (a bat file)"
-
   it "should create a folder for the project skeleton" do 
     path = 'spec/sandbox'
     taza_bin = "#{File.expand_path(File.dirname(__FILE__)+'/../bin/taza')} #{path}"
