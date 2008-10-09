65918866c2322a3ac597834667e33127f3fde66b
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-10-09 21:11:01 +0000

    added test for creating selenium drivers
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa_global/trunk/framework@322631 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/taza/browser.rb
+++ lib/taza/browser.rb
@@ -3,18 +3,20 @@ module Taza
     class << self
       attr_accessor :drivers
     end
-    self.drivers = {:watir => :create_watir, :firewatir => :create_firewatir}
+    self.drivers = {:watir => :create_watir, :firewatir => :create_firewatir, :selenium => :create_selenium}
 
     def self.create
       if(ENV['driver'].nil?)
-        self.create_selenium(ENV['browser'])
+        self.create_selenium
       else
         self.send(self.drivers[ENV['driver'].to_sym])
       end
     end
 
-
     def self.create_watir
+    end
+    def self.create_selenium
+      self.create_selenium_instance(ENV['browser'])
     end
   end
 end
--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -20,14 +20,24 @@ describe Taza::Browser do
     Taza::Browser.create
   end
   
-  it "should use selenium as the default driver" do
+  it "should be able to create a selenium driver" do
+    Taza::Browser.expects(:create_selenium)
+    ENV['driver'] = 'selenium'
+    Taza::Browser.create
+  end
+  
+  it "should use environment browser type when creating selenium" do
     browser_type = '*firefox'
-    Taza::Browser.expects(:create_selenium).with(browser_type)
+    Taza::Browser.expects(:create_selenium_instance).with(browser_type)
     ENV['browser'] = browser_type
     Taza::Browser.create
   end
 
-  it "should default to selenium as the default driver"  
+  it "should default to selenium as the default driver" do
+    Taza::Browser.expects(:create_selenium)
+    Taza::Browser.create
+  end
+  
   it "should get browser type from the config"
   it "should override config with environment setting"
   it "should default to a kind of browser"
