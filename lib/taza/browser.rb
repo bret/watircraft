c15d56c4cbfffe270ec8fa5821af5386eba481cf
rkarri <rkarri@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-10-09 23:06:42 +0000

    Finished pending specs for support watir browsers
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa_global/trunk/framework@322663 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/taza/browser.rb
+++ lib/taza/browser.rb
@@ -10,12 +10,27 @@ module Taza
     end
     
     def self.create_watir(browser)
-    
+      method = "create_watir_#{browser}"
+      raise BrowserUnsupportedError unless self.respond_to?(method)
+      self.send(method)
     end
-    
+
     def self.create_selenium(browser)
-      
     end
+
+    private    
+    def self.create_watir_ie
+    end
+    
+    def self.create_watir_firefox
+    end
+    
+    def self.create_watir_safari
+    end
+    
     
   end
+end
+
+class BrowserUnsupportedError < StandardError
 end
--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -4,11 +4,23 @@ require 'taza/browser'
 describe Taza::Browser do
   
   it "should be able to create a watir driver" do
-    Taza::Browser.expects(:create_watir)
-    Taza::Browser.create(:driver => :watir)
+    Taza::Browser.expects(:create_watir_ie)
+    Taza::Browser.create(:browser => :ie, :driver => :watir)
   end
   
-  it "should be able to create a firewatir driver"
+  it "should be able to create a firewatir driver" do
+    Taza::Browser.expects(:create_watir_firefox)
+    Taza::Browser.create(:browser => :firefox,:driver => :watir)
+  end
+  
+  it "should be able to create a safariwatir driver" do
+    Taza::Browser.expects(:create_watir_safari)
+    Taza::Browser.create(:browser => :safari,:driver => :watir)
+  end
+  
+  it "should raise unknown browser error for unsupported watir browsers" do
+    lambda { Taza::Browser.create(:browser => :foo_browser_9000,:driver => :watir) }.should raise_error(BrowserUnsupportedError)
+  end
   
   it "should use params browser type when creating selenium" do
     browser_type = :opera
@@ -25,5 +37,5 @@ describe Taza::Browser do
     Taza::Browser.expects(:create_selenium).with(:firefox)
     Taza::Browser.create
   end
-  it "should validate allowed browsers"
+  it "should raise selenium unsupported browser error"
 end
