138f9c38f8f1cad987002bf0affb3a844442fe69
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-10-10 22:39:54 +0000

    fixing the build
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa_global/trunk/framework@322823 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/taza_bin_spec.rb
+++ spec/taza_bin_spec.rb
@@ -6,17 +6,17 @@ require 'spec/spec_helper'
 describe "Taza project generator script" do
 
   after :each do
-    FileUtils.rm_rf('spec/sandbox')
+    FileUtils.rm_rf('spec/sandbox/generators')
   end
 
   it "should have an executable script" do
-    path = 'spec/sandbox'
+    path = 'spec/sandbox/generators'
     taza_bin = "#{File.expand_path(File.dirname(__FILE__)+'/../bin/taza')} #{path}"
     system("ruby #{taza_bin}").should be_true
   end
 
   it "should create a folder for the project skeleton" do 
-    path = 'spec/sandbox'
+    path = 'spec/sandbox/generators'
     taza_bin = "#{File.expand_path(File.dirname(__FILE__)+'/../bin/taza')} #{path}"
     system("ruby #{taza_bin}")
     File.directory?(path).should be_true
