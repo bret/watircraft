1a9f9b555dd321a1866b64394ee918a37e28ce4f
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-10-14 18:21:20 +0000

    fixing os bug in the config/vendorized_gems
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa_global/trunk/framework@323070 d1bde68c-af00-0410-813c-e860f6c31c7f

--- config/vendorized_gems.rb
+++ config/vendorized_gems.rb
@@ -4,4 +4,4 @@ gem_paths = [
   File.expand_path("#{File.dirname(__FILE__)}/../vendor/gems"),
   Gem.default_dir,
 ]
-Gem.send :set_paths, gem_paths.join(":")
+Gem.send :set_paths, gem_paths.join(File::PATH_SEPARATOR)
