72d57c2835442b8f0fb487d40f869f2eca5edf89
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-10-16 21:02:34 +0000

    added functional page spec template
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa_global/trunk/framework@323458 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/project_generator_spec.rb
+++ spec/project_generator_spec.rb
@@ -7,8 +7,9 @@ require 'taza/generators'
 describe Taza::Generators::Project do
 
   before :all do
-    @path = "./spec/sandbox/generated"
-    @file_name = "./spec/sandbox/generated/rakefile"
+    @path = File.join('.','spec','sandbox','generated')
+    @rake_file = File.join(@path,'rakefile')
+    @spec_helper = File.join(@path,'spec','spec_helper.rb')
   end
 
   after :each do
@@ -18,7 +19,13 @@ describe Taza::Generators::Project do
   it "should generate a rake file at a given path" do
     generator = Taza::Generators::Project.new(@path)
     generator.generate
-    File.exists?(@file_name).should be_true
+    File.exists?(@rake_file).should be_true
+  end
+  
+  it "should generate a page that can be required" do
+    generator = Taza::Generators::Project.new(@path)
+    generator.generate
+    system("ruby -c #{@spec_helper} > #{null_device}").should be_true
   end
 
 end
