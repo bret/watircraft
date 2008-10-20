388a7e27fb3fac4847531d7433289c7947d65ba3
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-10-20 18:32:03 +0000

    added rake task to run a sites functional specs
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@323790 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/taza/tasks.rb
+++ lib/taza/tasks.rb
@@ -27,6 +27,16 @@ namespace :spec do
   Spec::Rake::SpecTask.new :integration do |t|
     t.spec_files = 'spec/integration/**/*_spec.rb'
   end
+
+  namespace :functional do
+    Dir.glob('./spec/functional/*/').each do |dir|
+      site_name = File.basename(dir)
+      desc "run all functional specs for #{site_name}"
+      Spec::Rake::SpecTask.new site_name.to_sym do |t|
+        t.spec_files = "#{dir}**/*_spec.rb"
+      end
+    end
+  end
 end
 
 namespace :generate do
--- spec/taza_tasks_spec.rb
+++ spec/taza_tasks_spec.rb
@@ -1,16 +1,17 @@
 require 'spec/spec_helper'
 require 'rubygems'
+require 'rake'
 
 describe "Taza Tasks" do
-  
+
   before :all do
     @file_name ="./lib/taza/tasks.rb"
     @rake = Rake::Application.new
     Rake.application = @rake
   end
-  
+
   after :all do
-    Rake.application = nil 
+    Rake.application = nil
   end
 
   it "should create a rake task to generate a site" do
@@ -21,6 +22,12 @@ describe "Taza Tasks" do
   it "should create a rake task to generate a page" do
     load @file_name
     tasks.include?("generate:page").should be_true
+  end
+
+  it "should create rake spec tasks for all sites" do
+    Dir.expects(:glob).with('./spec/functional/*/').returns(['./spec/functional/foo/'])
+    load @file_name
+    tasks.include?("spec:functional:foo").should be_true
   end
 
   def tasks
