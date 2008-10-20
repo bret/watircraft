2d9b4c4f994b7ae43270f94bc78ae5fc83b52e3f
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-10-20 21:11:39 +0000

    added a taza env that gets set if not provided in the taza framework
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@323848 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/project_generator_spec.rb
+++ spec/project_generator_spec.rb
@@ -27,5 +27,20 @@ describe Taza::Generators::Project do
     generator.generate
     system("ruby -c #{@spec_helper} > #{null_device}").should be_true
   end
+  
+  it "spec helper should set the TAZA_ENV variable if it is not provided" do
+    ENV['TAZA_ENV'] = nil
+    generator = Taza::Generators::Project.new(@path)
+    generator.generate
+    load @spec_helper
+    ENV['TAZA_ENV'].should eql("isolation")
+  end
 
+  it "spec helper should not override the TAZA_ENV variable if was provided" do
+    ENV['TAZA_ENV'] = 'orange pie? is there such a thing?'
+    generator = Taza::Generators::Project.new(@path)
+    generator.generate
+    load @spec_helper
+    ENV['TAZA_ENV'].should eql('orange pie? is there such a thing?')
+  end
 end
