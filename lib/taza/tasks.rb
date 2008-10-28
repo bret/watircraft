74ce2531395c6bfe947a057c03c868318d1283ad
rkarri <rkarri@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-10-28 22:39:42 +0000

    TAZA : Added rake tasks to generate reports for the functional and integration tests
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@324959 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/taza/tasks.rb
+++ lib/taza/tasks.rb
@@ -4,23 +4,30 @@ require 'rubygems'
 require 'taglob/rake/tasks'
 require 'spec/rake/spectask'
 
-namespace :spec do
-  desc "run all functional specs"
+namespace :spec do  
+  FileUtils.mkdir("artifacts") unless File.directory?("artifacts")
+  desc "Run all functional specs "
   Spec::Rake::SpecTask.new :functional do |t|
     t.spec_files = 'spec/functional/**/*_spec.rb'
+    t.spec_opts=["--format html:artifacts/all_functional.html"]   
   end
-  desc "run all integration specs"
+  desc "Run all integration specs "
   Spec::Rake::SpecTask.new :integration do |t|
     t.spec_files = 'spec/integration/**/*_spec.rb'
+    t.spec_opts=["--format html:artifacts/all_integration.html"]    
   end
-  
+
   namespace :functional do
     Dir.glob('./spec/functional/*/').each do |dir|
       site_name = File.basename(dir)
-      desc "run all functional specs for #{site_name}"
+      desc "Run all functional specs for #{site_name} "
       Spec::Rake::SpecTask.new site_name.to_sym do |t|
         t.spec_files = "#{dir}**/*_spec.rb"
+        t.spec_opts=["--format html:artifacts/#{site_name}_functional.html"]        
       end
     end
-  end
+  end  
 end
+
+
+
