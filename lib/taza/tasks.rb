8513371b3b0df26dd12cc9fa2f8ac96713506ea0
rpothukuchi <rpothukuchi@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-11-03 22:46:14 +0000

    dry'd up the taza rake tasks
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@325649 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/taza/tasks.rb
+++ lib/taza/tasks.rb
@@ -5,16 +5,19 @@ require 'taglob/rake/tasks'
 require 'spec/rake/spectask'
 
 namespace :spec do  
+  def options(file_name)
+    ["--format","html:artifacts/#{file_name}.html","--format","-p"]
+  end
   FileUtils.mkdir("artifacts") unless File.directory?("artifacts")
   desc "Run all functional specs "
   Spec::Rake::SpecTask.new :functional do |t|
     t.spec_files = 'spec/functional/**/*_spec.rb'
-    t.spec_opts=["--format","html:artifacts/all_functional.html","--format","p"]   
+    t.spec_opts<<options("all_functional")
   end
   desc "Run all integration specs "
   Spec::Rake::SpecTask.new :integration do |t|
     t.spec_files = 'spec/integration/**/*_spec.rb'
-    t.spec_opts=["--format","html:artifacts/all_integration.html","--format","p"]    
+    t.spec_opts<<options("all_integration")
   end
 
   namespace :functional do
@@ -23,11 +26,8 @@ namespace :spec do
       desc "Run all functional specs for #{site_name} "
       Spec::Rake::SpecTask.new site_name.to_sym do |t|
         t.spec_files = "#{dir}**/*_spec.rb"
-        t.spec_opts<<["--format","html:artifacts/#{site_name}_functional.html","--format","-p"]
+        t.spec_opts<<options("#{site_name}_functional")
       end
     end
-  end  
+  end
 end
-
-
-
