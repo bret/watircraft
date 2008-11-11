2e8af72692e4a9a577a276c5b7759c7258bcdcdf
adanderson <adanderson@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-11-11 19:30:24 +0000

    Taza tasks will now generate html reports reflecting the structure of the spec folder
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@326831 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/taza/tasks.rb
+++ lib/taza/tasks.rb
@@ -4,30 +4,35 @@ require 'rubygems'
 require 'taglob/rake/tasks'
 require 'spec/rake/spectask'
 
-namespace :spec do  
-  def options(file_name)
-    ["--format","html:artifacts/#{file_name}.html","--format","-p"]
+namespace :spec do
+  def format_options(file_name)
+    file_name = "artifacts/#{file_name}/index.html"
+    dir_name = File.dirname(file_name)
+    FileUtils.mkdir_p(dir_name) unless File.directory?(dir_name)
+    ["--format","html:#{file_name}","--format","p"]
   end
-  FileUtils.mkdir("artifacts") unless File.directory?("artifacts")
-  desc "Run all functional specs "
+
+  desc "Run all functional specs"
   Spec::Rake::SpecTask.new :functional do |t|
     t.spec_files = 'spec/functional/**/*_spec.rb'
-    t.spec_opts<<options("all_functional")
+    t.spec_opts << format_options("functional/all")
   end
-  desc "Run all integration specs "
+  desc "Run all integration specs"
   Spec::Rake::SpecTask.new :integration do |t|
     t.spec_files = 'spec/integration/**/*_spec.rb'
-    t.spec_opts<<options("all_integration")
+    t.spec_opts << format_options("integration/all")
   end
 
   namespace :functional do
     Dir.glob('./spec/functional/*/').each do |dir|
       site_name = File.basename(dir)
-      desc "Run all functional specs for #{site_name} "
+
+      desc "Run all functional specs for #{site_name}"
       Spec::Rake::SpecTask.new site_name.to_sym do |t|
         t.spec_files = "#{dir}**/*_spec.rb"
-        t.spec_opts<<options("#{site_name}_functional")
+        t.spec_opts << format_options("functional/#{site_name}/all")
       end
+
     end
   end
 end
