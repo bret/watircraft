16eab828d4d54797c2277a5c6e097ce2d5b6799c
adanderson <adanderson@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-11-15 00:57:09 +0000

    Moved generator spec helper methods into its own modules
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@327354 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,3 +21,27 @@ def generator_sources
   [RubiGen::PathSource.new(:test, File.join(File.dirname(__FILE__),"..","lib", "app_generators")),
   RubiGen::PathSource.new(:test, File.join(File.dirname(__FILE__),"..", "generators"))]
 end
+
+module Helpers
+  module Generator
+    def generate_site(site_name)
+      run_generator('site', [site_name], generator_sources)
+      site_file_path = File.join(PROJECT_FOLDER,'lib','sites',"#{site_name.underscore}.rb")
+      require site_file_path
+      "::#{site_name.camelize}::#{site_name.camelize}".constantize.any_instance.stubs(:base_path).returns(PROJECT_FOLDER)
+      site_file_path
+    end
+  end
+  
+  module Taza
+    def stub_settings
+      ::Taza::Settings.stubs(:config).returns({})
+    end
+
+    def stub_browser
+      stub_browser = stub()
+      stub_browser.stubs(:goto)
+      ::Taza::Browser.stubs(:create).returns(stub_browser)
+    end
+  end
+end
