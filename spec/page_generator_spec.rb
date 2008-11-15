c66e2a0223aedaabab967f9862c59083a18a0466
adanderson <adanderson@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-11-15 00:57:11 +0000

    Fixed flow generator spec; added unique id to site generation to avoid false green lights
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@327355 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/page_generator_spec.rb
+++ spec/page_generator_spec.rb
@@ -10,14 +10,12 @@ describe "Page Generation" do
   include Helpers::Taza
 
   before :all do
-    @site_name = "Gap"
     @page_name = "CheckOut"
-    @page_functional_spec = File.join(PROJECT_FOLDER,'spec','functional','gap','check_out_page_spec.rb')
   end
 
   before :each do
     run_generator('taza', [APP_ROOT], generator_sources)
-    generate_site(@site_name)
+    @site_class = generate_site('Gap')
   end
 
   after :each do
@@ -36,24 +34,24 @@ describe "Page Generation" do
   end
 
   it "should generate a page spec that can be required" do
-    run_generator('page', [@page_name,@site_name], generator_sources)
-    system("ruby -c #{@page_functional_spec} > #{null_device}").should be_true
+    run_generator('page', [@page_name,@site_class.to_s], generator_sources)
+    page_functional_spec = File.join(PROJECT_FOLDER,'spec','functional',@site_class.to_s.underscore,'check_out_page_spec.rb')
+    system("ruby -c #{page_functional_spec} > #{null_device}").should be_true
   end
 
   it "should be able to access the generated page from the site" do
-    run_generator('page', [@page_name,@site_name], generator_sources)
+    run_generator('page', [@page_name,@site_class.to_s], generator_sources)
     stub_settings
     stub_browser
-    @site_name.constantize.new.check_out_page
+    @site_class.new.check_out_page
   end
 
   it "should be able to access the generated page for its site" do
     stub_browser
     stub_settings
-    new_site_name = "Pag"
-    generate_site(new_site_name)
-    run_generator('page', [@page_name,@site_name], generator_sources)
-    run_generator('page', [@page_name,new_site_name], generator_sources)
-    Pag.new.check_out_page.class.should_not eql(Gap.new.check_out_page.class)
+    new_site_class = generate_site('Pag')
+    run_generator('page', [@page_name,@site_class.to_s], generator_sources)
+    run_generator('page', [@page_name,new_site_class.to_s], generator_sources)
+    new_site_class.new.check_out_page.class.should_not eql(@site_class.new.check_out_page.class)
   end
 end
