e1fef03b6c2ede6d4fdd0e1d2664f152904688a0
adanderson <adanderson@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-11-17 18:43:23 +0000

    Rest of site specs are fixed, still need to transition to generators
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@327405 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/sandbox/pages/foo/bar.rb
+++ spec/sandbox/pages/foo/bar.rb
@@ -1,11 +1,9 @@
-module FooSite
-  class Bar < Taza::Page
-    element :foo do
-      browser
-    end
+class Bar < Taza::Page
+  element :foo do
+    browser
+  end
 
-    filter :name => :baz, :elements => [:foo] do
-      true
-    end
+  filter :name => :baz, :elements => [:foo] do
+    true
   end
 end
