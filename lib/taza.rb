edaad2b54d4de0a84b94747d2ed3caf0561f72e1
Adam Anderson <adamandersonis@gmail.com>
2008-11-23 09:38:02 -0800

    instance_exec will only be added to Object for Ruby versions <= 1.8.6

--- lib/taza.rb
+++ lib/taza.rb
@@ -6,7 +6,7 @@ require 'taza/flow'
 
 module Taza
   VERSION = '0.8.0'
-  
+
   def self.windows?
     PLATFORM.include?("mswin")  
   end 
@@ -27,23 +27,26 @@ module ForwardInitialization
   end
 end
 
-class Object
-  module InstanceExecHelper; end
-  include InstanceExecHelper
-  def instance_exec(*args, &block)
-    begin
-      old_critical, Thread.critical = Thread.critical, true
-      n = 0
-      n += 1 while respond_to?(mname="__instance_exec#{n}")
-      InstanceExecHelper.module_eval{ define_method(mname, &block) }
-    ensure
-      Thread.critical = old_critical
+# instance_exec comes with >1.8.7 thankfully
+if VERSION <= '1.8.6'
+  class Object
+    module InstanceExecHelper; end
+    include InstanceExecHelper
+    def instance_exec(*args, &block)
+      begin
+        old_critical, Thread.critical = Thread.critical, true
+        n = 0
+        n += 1 while respond_to?(mname="__instance_exec#{n}")
+        InstanceExecHelper.module_eval{ define_method(mname, &block) }
+      ensure
+        Thread.critical = old_critical
+      end
+      begin
+        ret = send(mname, *args)
+      ensure
+        InstanceExecHelper.module_eval{ remove_method(mname) } rescue nil
+      end
+      ret
     end
-    begin
-      ret = send(mname, *args)
-    ensure
-      InstanceExecHelper.module_eval{ remove_method(mname) } rescue nil
-    end
-    ret
   end
 end
