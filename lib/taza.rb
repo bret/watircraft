2b1c4396965ceed3bcaa3681c13bf7bf2ad7733a
Drew Olson <drew@drewolson.org>
2008-12-02 22:46:20 -0500

    instance_exec moved to lib/extensions/object.rb
    * object_spec.rb specs instance_exec behavior

--- lib/taza.rb
+++ lib/taza.rb
@@ -3,6 +3,7 @@ require 'taza/site'
 require 'taza/browser'
 require 'taza/settings'
 require 'taza/flow'
+require 'extensions/object'
 
 module Taza
   VERSION = '0.8.0'
@@ -27,26 +28,3 @@ module ForwardInitialization
   end
 end
 
-# instance_exec comes with >1.8.7 thankfully
-if VERSION <= '1.8.6'
-  class Object
-    module InstanceExecHelper; end
-    include InstanceExecHelper
-    def instance_exec(*args, &block)
-      begin
-        old_critical, Thread.critical = Thread.critical, true
-        n = 0
-        n += 1 while respond_to?(mname="__instance_exec#{n}")
-        InstanceExecHelper.module_eval{ define_method(mname, &block) }
-      ensure
-        Thread.critical = old_critical
-      end
-      begin
-        ret = send(mname, *args)
-      ensure
-        InstanceExecHelper.module_eval{ remove_method(mname) } rescue nil
-      end
-      ret
-    end
-  end
-end
