9285dc0aede71b058af9d6d4409ae11654c1e79f
adanderson <adanderson@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-04 19:11:29 +0000

    instance_exec moved to lib/extensions/object.rb
    * object_spec.rb specs instance_exec behavior
    
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@329922 d1bde68c-af00-0410-813c-e860f6c31c7f

---
+++ lib/extensions/object.rb
@@ -0,0 +1,23 @@
+# instance_exec comes with >1.8.7 thankfully
+if VERSION <= '1.8.6'
+  class Object
+    module InstanceExecHelper; end
+    include InstanceExecHelper
+    def instance_exec(*args, &block)
+      begin
+        old_critical, Thread.critical = Thread.critical, true
+        n = 0
+        n += 1 while respond_to?(mname="__instance_exec#{n}")
+        InstanceExecHelper.module_eval{ define_method(mname, &block) }
+      ensure
+        Thread.critical = old_critical
+      end
+      begin
+        ret = send(mname, *args)
+      ensure
+        InstanceExecHelper.module_eval{ remove_method(mname) } rescue nil
+      end
+      ret
+    end
+  end
+end
