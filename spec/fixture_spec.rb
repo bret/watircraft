cef93f0369e5588a5cd02e7ef36ee6f246cc2672
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-09 23:15:49 +0000

    added a fixture_methods module to include to get all fixtures in a describe or test
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@330626 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/fixture_spec.rb
+++ spec/fixture_spec.rb
@@ -14,5 +14,18 @@ describe Taza::Fixture do
     example.name.should eql("first")
     example.price.should eql(1)
   end
+
+  it "should know if it has a fixture with a given name" do
+    fixture = Taza::Fixture.new
+    fixture.load_all
+    fixture.has_fixture_file?(:examples).should be_true
+    fixture.has_fixture_file?(:foo).should be_false
+  end
+
+  include Taza::Fixture_methods
+
+  it "should be able to look up a fixture entity off fixture_methods module" do
+    examples(:first_example).name.should eql('first')
+  end
   
 end
