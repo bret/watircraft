2771dcf8cdd6e5499ccfd7376c036db27832d088
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-10 23:57:09 +0000

    started to expand out the one to one relationship in fixtures
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@330956 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/fixture_spec.rb
+++ spec/fixture_spec.rb
@@ -7,7 +7,7 @@ describe Taza::Fixture do
     Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox')
     fixture = Taza::Fixture.new
     fixture.load_all
-    example = fixture.fixtures(:examples)['first_example']
+    example = fixture.get_fixture_entity('examples','first_example')
     example.name.should eql("first")
     example.price.should eql(1)
   end
@@ -24,11 +24,20 @@ describe Taza::Fixture do
     Taza::Fixture.new.base_path.should eql('./spec')
   end
 
-  it "should use the spec folder as the base path" do
+  it "should know if a pluralized fixture of that name exists" do
     Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox')
     fixture = Taza::Fixture.new
     fixture.load_all
     fixture.pluralized_fixture_exists?('example').should be_true
+    fixture.pluralized_fixture_exists?('foo').should be_false
+  end
+
+  it "should use the spec folder as the base path" do
+    Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox')
+    fixture = Taza::Fixture.new
+    fixture.load_all
+    hash = fixture.replace_one_to_one_relationship_keys_with_values({'user' => 'shatner'})
+    hash['user'].name.should eql("William Shatner")
   end
   
 end
