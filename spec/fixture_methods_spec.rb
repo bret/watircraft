e1c8c47712cb8a4337b25c356371812437496e5f
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-11 17:36:14 +0000

    renamed hash keys to methods method
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@331046 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/fixture_methods_spec.rb
+++ spec/fixture_methods_spec.rb
@@ -16,6 +16,7 @@ describe Taza::Fixture_methods do
     lambda{zomgwtf(:first_example)}.should raise_error(NoMethodError)
   end
   
+  #TODO: this test tests what is in entity's instance eval not happy with it being here
   it "should be able to look up a fixture entity off fixture_methods module" do
     examples(:first_example).user.name.should eql(users(:shatner).name)
   end
