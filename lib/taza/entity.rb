d74b86c4b026d7845f7beafe83d7ca803e66dc01
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-11 19:36:06 +0000

    renamed fixtures to fixture_names
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@331091 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/taza/entity.rb
+++ lib/taza/entity.rb
@@ -1,19 +1,21 @@
 module Taza
   class Entity
+   
     def initialize(hash,fixture)
       @hash = hash
       @fixture = fixture
       @hash.keys.each do |key|
-        self.instance_eval <<-EOS
-        def #{key}
-          if(@fixture.pluralized_fixture_exists?('#{key}'))
-            @fixture.get_fixture_entity('#{key}'.pluralize_to_sym,@hash['#{key}'])
-          else
-            @hash['#{key}']
+        self.instance_eval do
+          define_method(key) do
+            if(@fixture.pluralized_fixture_exists?(key))
+              @fixture.get_fixture_entity(key.pluralize_to_sym,@hash[key])
+            else
+              @hash[key]
+            end
           end
         end
-        EOS
       end
     end
+    
   end
 end
