710074d809871a8d89112486b9fa505300fe0511
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-11 18:39:06 +0000

    renaming fixture_methods to fixtures
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@331060 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/fixture_methods_spec.rb
+++ spec/fixture_methods_spec.rb
@@ -1,12 +1,12 @@
 require 'spec/spec_helper'
 require 'taza/fixture'
-describe Taza::Fixture_methods do
+describe Taza::Fixtures do
   
   before :each do
     Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox')
   end
   
-  include Taza::Fixture_methods
+  include Taza::Fixtures
   
   it "should be able to look up a fixture entity off fixture_methods module" do
     examples(:first_example).name.should eql('first')
