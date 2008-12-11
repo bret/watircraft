85c3fc2b60a455eb01def1dccb35460ec7e978cb
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-11 19:37:10 +0000

    accidently commited, reverting change
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@331092 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/taza/entity.rb
+++ lib/taza/entity.rb
@@ -1,21 +1,19 @@
 module Taza
   class Entity
-   
     def initialize(hash,fixture)
       @hash = hash
       @fixture = fixture
       @hash.keys.each do |key|
-        self.instance_eval do
-          define_method(key) do
-            if(@fixture.pluralized_fixture_exists?(key))
-              @fixture.get_fixture_entity(key.pluralize_to_sym,@hash[key])
-            else
-              @hash[key]
-            end
+        self.instance_eval <<-EOS
+        def #{key}
+          if(@fixture.pluralized_fixture_exists?('#{key}'))
+            @fixture.get_fixture_entity('#{key}'.pluralize_to_sym,@hash['#{key}'])
+          else
+            @hash['#{key}']
           end
         end
+        EOS
       end
     end
-    
   end
 end
