2e19cffeb6c2d5cf3edd0d462274575fbd8675f0
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-11 00:45:30 +0000

    readded entity, hash extension method now returns a entity
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@330973 d1bde68c-af00-0410-813c-e860f6c31c7f

---
+++ lib/taza/entity.rb
@@ -0,0 +1,19 @@
+module Taza
+  class Entity
+    def initialize(hash,fixture)
+      @hash = hash
+      @fixture = fixture
+      @hash.keys.each do |key|
+        self.instance_eval <<-EOS
+        def #{key}
+          if(@fixture.pluralized_fixture_exists?('#{key}'))
+            @fixture.get_fixture_entity('#{key}',@hash['#{key}'])
+          else
+            @hash['#{key}']
+          end
+        end
+        EOS
+      end
+    end
+  end
+end
