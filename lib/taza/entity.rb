0477a2aa747c2be42790565be56bf69eb9631408
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-11 19:44:18 +0000

    removed the string eval from entity
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@331099 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/taza/entity.rb
+++ lib/taza/entity.rb
@@ -5,19 +5,23 @@ module Taza
       @fixture = fixture
       define_methods_for_hash_keys
     end
-    
+
     def define_methods_for_hash_keys
       @hash.keys.each do |key|
-        self.instance_eval <<-EOS
-        def #{key}
-          if(@fixture.pluralized_fixture_exists?('#{key}'))
-            @fixture.get_fixture_entity('#{key}'.pluralize_to_sym,@hash['#{key}'])
+        create_method(key) do
+          if(@fixture.pluralized_fixture_exists?(key))
+            @fixture.get_fixture_entity(key.pluralize_to_sym,@hash[key])
           else
-            @hash['#{key}']
+            @hash[key]
           end
         end
-        EOS
       end
     end
+
+    private
+    def create_method(name, &block)
+      self.class.send(:define_method, name, &block)
+    end
+
   end
 end
