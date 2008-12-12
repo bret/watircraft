9773c11e674df562ab5b43449b8a8eab155a5ec3
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-12 00:51:30 +0000

    removed un-needed method from fixture
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@331345 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/fixture_spec.rb
+++ spec/fixture_spec.rb
@@ -12,14 +12,6 @@ describe Taza::Fixture do
     example.price.should eql(1)
   end
 
-  it "should know if it has a fixture with a given name" do
-    Taza::Fixture.any_instance.stubs(:base_path).returns('./spec/sandbox')
-    fixture = Taza::Fixture.new
-    fixture.load_all
-    fixture.has_fixture_file?(:examples).should be_true
-    fixture.has_fixture_file?(:foo).should be_false
-  end
-
   it "should use the spec folder as the base path" do
     Taza::Fixture.new.base_path.should eql('./spec')
   end
