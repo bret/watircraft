bc0d29494898375170bd97b88291eb4f13f1bb00
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-17 23:12:03 +0000

    added rdoc for string extensions
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@332047 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/extensions/string.rb
+++ lib/extensions/string.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'activesupport'
 
 class String
+  # pluralizes a string and turns it into a symbol
+  # Example:
+  #  "apple".pluralize_to_sym    # => :apples
   def pluralize_to_sym
     self.pluralize.to_sym
   end
