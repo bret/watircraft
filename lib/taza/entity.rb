47d8fb824b12bda3f66fd466dc60f8171ab7be20
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-17 22:58:46 +0000

    added rdoc for entity
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@332042 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/taza/entity.rb
+++ lib/taza/entity.rb
@@ -1,11 +1,13 @@
 module Taza
   class Entity
+    #Creates a entity, pass in a hash to be methodized and the fixture to look up other fixtures (not entirely happy with this abstraction)
     def initialize(hash,fixture)
       @hash = hash
       @fixture = fixture
       define_methods_for_hash_keys
     end
-
+    
+    #This method converts hash keys into methods onto the entity
     def define_methods_for_hash_keys
       @hash.keys.each do |key|
         create_method(key) do
@@ -14,7 +16,8 @@ module Taza
       end
     end
 
-    def get_value_for_entry(key)
+    #This method will lookup another fixture if a pluralized fixture exists otherwise return the value in the hash
+    def get_value_for_entry(key) # :nodoc:
       if @fixture.pluralized_fixture_exists?(key)
         @fixture.get_fixture_entity(key.pluralize_to_sym,@hash[key])
       else
@@ -23,7 +26,7 @@ module Taza
     end
 
     private
-    def create_method(name, &block)
+    def create_method(name, &block) # :nodoc:
       self.class.send(:define_method, name, &block)
     end
 
