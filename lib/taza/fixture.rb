91788d27e5edb8fcb615c3ee2e911b5d8fa6133f
nevans <nevans@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-17 22:54:12 +0000

    added rdoc to fixture
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@332038 d1bde68c-af00-0410-813c-e860f6c31c7f

--- lib/taza/fixture.rb
+++ lib/taza/fixture.rb
@@ -1,11 +1,11 @@
 module Taza
-  class Fixture
+  class Fixture # :nodoc:
 
-    def initialize
+    def initialize # :nodoc:
       @fixtures = {}
     end
 
-    def load_all
+    def load_all # :nodoc:
       Dir.glob(fixtures_pattern) do |file|
         entitized_fixture = {}
         YAML.load_file(file).each do |key, value|
@@ -15,29 +15,42 @@ module Taza
       end
     end
     
-    def fixture_names
+    def fixture_names # :nodoc:
       @fixtures.keys
     end
     
-    def get_fixture_entity(fixture_file_key,entity_key)
+    def get_fixture_entity(fixture_file_key,entity_key) # :nodoc:
       @fixtures[fixture_file_key][entity_key]
     end
 
-    def pluralized_fixture_exists?(singularized_fixture_name)
+    def pluralized_fixture_exists?(singularized_fixture_name) # :nodoc:
       fixture_names.include?(singularized_fixture_name.pluralize_to_sym)
     end
 
-    def fixtures_pattern
+    def fixtures_pattern # :nodoc:
       File.join(base_path, 'fixtures','*.yml')
     end
 
-    def base_path
+    def base_path # :nodoc:
       File.join('.','spec')
     end
   end
   
+  # The module that will mixin methods based on the fixture files in your 'spec/fixtures'
+  # 
+  # Example:
+  #   describe "something" do
+  #     it "should test something" do
+  #       users(:jane_smith).first_name.should eql("jane")
+  #     end
+  #   end
+  # 
+  #  where there is a spec/fixtures/users.yml file containing a entry of:
+  # jane_smith:
+  #   first_name: jane
+  #   last_name: smith
   module Fixtures
-    def Fixtures.included(other_module)
+    def Fixtures.included(other_module) # :nodoc:
       fixture = Fixture.new
       fixture.load_all
       fixture.fixture_names.each do |fixture_name|
