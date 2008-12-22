849bb64dfcf49038de62254ddcdcd00e9e21794e
adanderson <adanderson@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-22 22:40:04 +0000

    Updating Rubigen dependency to 1.4.0 Fixes bug where installed gem generators would be used over local development gems.
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@332495 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/flow_generator_spec.rb
+++ spec/flow_generator_spec.rb
@@ -2,7 +2,7 @@ require 'spec/spec_helper'
 require 'rubygems'
 require 'fileutils'
 require 'taza'
-require 'vendor/gems/gems/rubigen-1.3.2/test/test_generator_helper'
+require 'vendor/gems/gems/rubigen-1.4.0/test/test_generator_helper'
 
 class Taza::Site
   def flows
--- spec/project_generator_spec.rb
+++ spec/project_generator_spec.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 require 'rake'
 require 'fileutils'
 require 'taza'
-require 'vendor/gems/gems/rubigen-1.3.2/test/test_generator_helper'
+require 'vendor/gems/gems/rubigen-1.4.0/test/test_generator_helper'
 
 describe "Project Generator" do
   include RubiGen::GeneratorTestHelper
