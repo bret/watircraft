fc4bf97a993fea72429e155064d6014b2cb9a9ac
adanderson <adanderson@d1bde68c-af00-0410-813c-e860f6c31c7f>
2008-12-22 22:40:31 +0000

    Simplified create_watir and also vendorized commonwatir
    git-svn-id: http://svn.gid.gap.com/svn/gid/qa/trunk/taza@332510 d1bde68c-af00-0410-813c-e860f6c31c7f

--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -2,6 +2,7 @@ require 'spec/spec_helper'
 require 'taza/browser'
 require 'taza/settings'
 require 'selenium'
+require 'watir'
 
 describe Taza::Browser do
 
@@ -20,57 +21,8 @@ describe Taza::Browser do
     end
   end
 
-  describe "Taza::Browser with Safari Watir" do
-    it "should be able to make a safari watir instance" do
-      Taza::Browser.expects(:require).with('safariwatir')
-      browser = Taza::Browser.create({:browser => :safari, :driver => :watir,:url =>'http://www.google.com'})
-      browser.should be_a_kind_of(Watir::Safari)
-    end
-  end
-
-  module Watir
-    class IE
-    end
-  end
-
-  describe "Taza::Browser with watir ie" do
-    it "should be able to make watir ie instance" do
-      Taza::Browser.expects(:require).with('watir')
-      browser =  Taza::Browser.create({:browser => :ie,:driver =>:watir,:url => 'http://www.google.com'})
-      browser.should be_a_kind_of(Watir::IE)
-    end
-  end
-
-  module FireWatir
-    class Firefox
-    end
-  end
-
-  describe "Taza::Browser with watir firefox" do
-    it "should be able to make watir firefox instance" do
-      Taza::Browser.expects(:require).with('firewatir')
-      browser =  Taza::Browser.create({:browser => :firefox,:driver =>:watir,:url => 'http://www.google.com'})
-      browser.should be_a_kind_of(FireWatir::Firefox)
-    end
-  end
-
-  it "should be able to create a watir driver" do
-    Taza::Browser.expects(:watir_ie).returns(Object)
-    Taza::Browser.create(:browser => :ie, :driver => :watir)
-  end
-
-  it "should be able to create a firewatir driver" do
-    Taza::Browser.expects(:watir_firefox).returns(Object)
-    Taza::Browser.create(:browser => :firefox,:driver => :watir)
-  end
-
-  it "should be able to create a safariwatir driver" do
-    Taza::Browser.expects(:watir_safari).returns(Object)
-    Taza::Browser.create(:browser => :safari,:driver => :watir)
-  end
-
   it "should raise unknown browser error for unsupported watir browsers" do
-    lambda { Taza::Browser.create(:browser => :foo_browser_9000,:driver => :watir) }.should raise_error(Taza::BrowserUnsupportedError)
+    lambda { Taza::Browser.create(:browser => :foo_browser_9000,:driver => :watir) }.should raise_error(StandardError)
   end
 
   it "should use params browser type when creating selenium" do
