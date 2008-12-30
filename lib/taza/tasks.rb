ef8d797288f077bff246b0c8b8bb1b942fa07b46
Bret Pettichord <bret@pettichord.com>
2008-12-30 23:30:19 +0000

    remove integration test task and functional test subtasks

--- lib/taza/tasks.rb
+++ lib/taza/tasks.rb
@@ -21,32 +21,6 @@ namespace :spec do
     t.spec_files = Dir.taglob('spec/functional/**/*_spec.rb',tags)
     t.spec_opts << format_options("functional/all")
   end
-  desc "Run all integration specs"
-  Spec::Rake::SpecTask.new :integration do |t|
-    t.spec_files = Dir.taglob('spec/integration/**/*_spec.rb',tags)
-    t.spec_opts << format_options("integration/all")
-  end
-
-  namespace :functional do
-    Dir.glob('./spec/functional/*/').each do |dir|
-      site_name = File.basename(dir)
-      desc "Run all functional specs for #{site_name}"
-      Spec::Rake::SpecTask.new site_name.to_sym do |t|
-        t.spec_files = Dir.taglob("#{dir}**/*_spec.rb",tags)
-        t.spec_opts << format_options("functional/#{site_name}/all")
-      end
-      namespace site_name.to_sym do
-        Dir.glob("./spec/functional/#{site_name}/*_spec.rb").each do |page_spec_file|
-          page_spec_name = File.basename(page_spec_file)
-          page_name = page_spec_name.chomp('_spec.rb')
-          Spec::Rake::SpecTask.new page_name.to_sym do |t|
-            t.spec_files = page_spec_file
-            t.spec_opts << format_options("functional/#{site_name}/#{page_name}")
-          end
-        end
-      end
-    end
-  end
 end
 
 require 'cucumber/rake/task'
