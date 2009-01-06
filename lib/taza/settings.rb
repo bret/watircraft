7f9294c7edd920efad7f8958adae6d365114a834
Bret Pettichord <bret@pettichord.com>
2009-01-06 23:37:11 +0000

    pure refactor of Taza::Settings to clarify current behavior

--- lib/taza/settings.rb
+++ lib/taza/settings.rb
@@ -2,43 +2,46 @@ require 'activesupport'
 
 module Taza
   class Settings
-    # The config settings for a site.yml file.  ENV variables will override the settings:
-    #  Can override the browser in config via ENV['BROWSER']
-    #  Can override the driver in config via ENV['DRIVER']
-    #  Can override the timeout in config via ENV['TIMEOUT']
-    #  Can override the server_ip in config via ENV['SERVER_IP']
-    #  Can override the server_port in config via ENV['SERVER_PORT']
+    # The config settings from the site.yml and config.yml files.  
+    # ENV variables will override the settings.
     #
     # Example:
-    #   Taza::Settings.Config('google')
+    #   Taza::Settings.config('google')
     def self.config(site_name)
       env_settings = {}
-      env_settings[:browser] = ENV['BROWSER'].to_sym if ENV['BROWSER']
-      env_settings[:driver]  = ENV['DRIVER'].to_sym if ENV['DRIVER']
-      env_settings[:timeout] = ENV['TIMEOUT'] if ENV['TIMEOUT']
-      env_settings[:server_ip] = ENV['SERVER_IP'] if ENV['SERVER_IP']
-      env_settings[:server_port] = ENV['SERVER_PORT'] if ENV['SERVER_PORT']
-      env_settings = {:browser=>:firefox,:driver=>:selenium}.merge(config_file.merge(env_settings))
-      site_file(site_name).merge(env_settings)
+      keys = %w(browser driver timeout server_ip server_port)
+      keys.each do |key|
+        env_settings[key.to_sym] = ENV[key.upcase] if ENV[key.upcase]
+      end
+      
+      default_settings = {:browser => :firefox, :driver => :selenium}
+      
+      # Because of the way #merge works, the settings at the bottom of the list,
+      # trump those at the top.
+      settings = site_file(site_name).merge(
+                   default_settings.merge(
+                     config_file.merge(
+                       env_settings)))
+
+      settings[:browser] = settings[:browser].to_sym
+      settings[:driver] = settings[:driver].to_sym
+
+      settings
     end
 
-    # Loads the config file for the entire project and returns the hash.
-    # Does not override settings from the ENV variables.
+    # Returns a hash corresponding to the project config file.
     def self.config_file
       return {} unless File.exists?(config_file_path)
       YAML.load_file(config_file_path)
     end
 
     def self.config_file_path # :nodoc:
-      File.join(config_folder, 'config.yml')
+      File.join(path, 'config', 'config.yml')
     end
     
-    def self.config_folder # :nodoc:
-      File.join(path, 'config')
-    end
-    
+    # Returns a hash for the currently specified test environment
     def self.site_file(site_name) # :nodoc:
-      YAML.load_file(File.join(config_folder,"#{site_name.underscore}.yml"))[ENV['TAZA_ENV']]
+      YAML.load_file(File.join(path, 'config', "#{site_name.underscore}.yml"))[ENV['TAZA_ENV']]
     end
 
     def self.path # :nodoc:
