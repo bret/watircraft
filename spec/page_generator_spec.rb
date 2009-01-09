a370827666784a55df1a128544b4da98b8989690
Bret Pettichord <bret@pettichord.com>
2009-01-09 16:58:47 +0000

    cleaned up page generator spec

--- spec/page_generator_spec.rb
+++ spec/page_generator_spec.rb
@@ -8,13 +8,10 @@ describe "Page Generation" do
   include Helpers::Generator
   include Helpers::Taza
 
-  before :all do
-    @page_name = "check out"
-  end
-
   before :each do
     generate_project
     @site_class = generate_site('Gap')
+    @site_name = @site_class.to_s
   end
 
   after :each do
@@ -29,26 +26,26 @@ describe "Page Generation" do
   end
 
   it "should give you usage if you provide two arguments" do
-    lambda { run_generator('page', [@page_name, 'extra'], generator_sources) }.
+    lambda { run_generator('page', ['simple', 'extra'], generator_sources) }.
       should raise_error(RubiGen::UsageError)
   end
 
   it "should display an error if no site is specified in the config.yml" do
     PageGenerator.any_instance.stubs(:configured_site).returns(nil)
-    lambda { run_generator('page', [@page_name], generator_sources) }.
+    lambda { run_generator('page', ['simple'], generator_sources) }.
       should raise_error(RubiGen::UsageError, "Error. A site must first be specified in config.yml")
   end
 
   it "should display an error if the site in config.yml can't be found" do
     PageGenerator.any_instance.stubs(:configured_site).returns('no_such_site')
-    lambda { run_generator('page', [@page_name], generator_sources) }.
+    lambda { run_generator('page', ['simple'], generator_sources) }.
       should raise_error(RubiGen::UsageError, /Error\. Site file .*lib\/no_such_site.rb not found\./)
   end
     
   # Positive
 
   it "should be able to access the generated page from the site" do
-    PageGenerator.any_instance.stubs(:configured_site).returns(@site_class.to_s)    
+    PageGenerator.any_instance.stubs(:configured_site).returns(@site_name)    
     run_generator('page', ['simple'], generator_sources)
     stub_settings
     stub_browser
@@ -56,7 +53,7 @@ describe "Page Generation" do
   end
 
   it "should be able to access the generated page when it has a space" do
-    PageGenerator.any_instance.stubs(:configured_site).returns(@site_class.to_s)    
+    PageGenerator.any_instance.stubs(:configured_site).returns(@site_name)    
     run_generator('page', ['check out'], generator_sources)
     stub_settings
     stub_browser
@@ -64,15 +61,15 @@ describe "Page Generation" do
   end
 
   it "should be able to access the generated page when it has an underscore" do
-    PageGenerator.any_instance.stubs(:configured_site).returns(@site_class.to_s)    
-    run_generator('page', ['check_out'], generator_sources)
+    PageGenerator.any_instance.stubs(:configured_site).returns(@site_name)    
+    run_generator('page', ['check_any'], generator_sources)
     stub_settings
     stub_browser
-    @site_class.new.check_out_page
+    @site_class.new.check_any_page
   end
   
   it "should be able to generate a page using the project default" do
-    lambda{run_generator('page', [@page_name], generator_sources)}.
+    lambda{run_generator('page', ['simple'], generator_sources)}.
       should_not raise_error
   end
     
