5b49362318b568c2871e0fa1e45707703b8511fc
Bret Pettichord <bret@pettichord.com>
2009-01-11 21:06:43 +0000

    add display_value methods to watir classes

---
+++ lib/extensions/watir.rb
@@ -0,0 +1,36 @@
+# TODO: support radiogroup
+# TODO: migrate this code into Watir
+# TODO: also handle FireWatir classes
+module Watir
+  class TextField # includes Hidden
+    def display_value
+      value
+    end
+  end
+  class FileField
+    def display_value
+      value
+    end
+  end
+  class CheckBox
+    def display_value
+      checked?
+    end
+  end
+  class SelectList
+    # Note: currently works for single-select only    
+    def display_value
+      getSelectedItems[0]
+    end
+  end
+  class Button
+    def display_value
+      value
+    end
+  end
+  class NonControlElement
+    def display_value
+      text
+    end
+  end
+end
