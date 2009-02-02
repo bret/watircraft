e84bf784a44504bd63265d961f901f8d03ecd3cb
Bret Pettichord <bret@pettichord.com>
2009-02-02 20:08:02 +0000

    add tests of feature_helper and rakefile (test for latter was not actually testing the rakefile).

--- spec/project_generator_spec.rb
+++ spec/project_generator_spec.rb
@@ -8,8 +8,9 @@ describe "Project Generator" do
   include RubiGen::GeneratorTestHelper
 
   before :all do
-    @spec_helper = File.join(TMP_ROOT, PROJECT_NAME, 'test', 'specs', 'spec_helper.rb')
-    @rakefile = File.join(TMP_ROOT,PROJECT_NAME,'rakefile')
+    @spec_helper = File.join(TMP_ROOT, PROJECT_NAME, 'test/specs/spec_helper.rb')
+    @feature_helper = File.join(TMP_ROOT, PROJECT_NAME, 'test/features/feature_helper.rb')
+    @rakefile = File.join(TMP_ROOT,PROJECT_NAME, 'rakefile')
   end
 
   before :each do
@@ -26,9 +27,14 @@ describe "Project Generator" do
     system("ruby -c #{@spec_helper} > #{null_device}").should be_true
   end
 
+  it "should generate a spec helper that can be required" do
+    run_generator('watircraft', [APP_ROOT], generator_sources)
+    system("ruby -c #{@feature_helper} > #{null_device}").should be_true
+  end
+
   it "should generate a rakefile that can be required" do
     run_generator('watircraft', [APP_ROOT], generator_sources)
-    system("ruby -c #{@spec_helper} > #{null_device}").should be_true
+    system("ruby -c #{@rakefile} > #{null_device}").should be_true
   end
 
   it "spec helper should set the ENVIRONMENT variable if it is not provided" do
