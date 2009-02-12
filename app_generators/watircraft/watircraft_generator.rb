fccbadf68d3207ac96d3befb562ef08ee66ebdb1
Bret Pettichord <bret@pettichord.com>
2009-02-12 23:26:49 +0000

    broke off spec_initialize from spec helper

--- app_generators/watircraft/watircraft_generator.rb
+++ app_generators/watircraft/watircraft_generator.rb
@@ -37,12 +37,17 @@ class WatircraftGenerator < RubiGen::Base
   def manifest
     record do |m|    
       @@new_directories.each { |path| m.directory path }
+
       m.file "rakefile.rb", "rakefile", :collision => :force
       m.template "config.yml.erb", "config/config.yml", :collision => :skip
-      m.template "spec_helper.rb.erb", "test/specs/spec_helper.rb", :collision => :force
+
+      m.file "spec_helper.rb", "test/specs/spec_helper.rb", :collision => :force
       m.file "feature_helper.rb", "test/features/feature_helper.rb", :collision => :force
       m.template "initialize.rb.erb", "lib/initialize.rb", :collision => :force
+
+      m.template "spec_initialize.rb.erb", "lib/spec_initialize.rb", :collision => :force
       m.template "world.rb.erb", "lib/steps/world.rb", :collision => :force
+
       m.dependency "install_rubigen_scripts", [destination_root, 'watircraft'],
         :shebang => options[:shebang], :collision => :force
       m.dependency "site", [@site], :destination => destination_root
