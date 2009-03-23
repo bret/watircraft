988e3caa3ea6bd9fed37ac38f0b995cc6dec3bc5
Bret Pettichord <bret@pettichord.com>
2009-03-23 20:03:53 +0000

    generalize test, so it can be used for firewatir too

--- spec/watir_spec.rb
+++ spec/watir_spec.rb
@@ -4,17 +4,26 @@ require 'watir/ie'
 require 'extensions/watir'
 
 describe 'Watir Extensions' do
-  it "should provide a display_value method for text fields" do
-    container = stub()
-    container.stubs(:page_container)
-    tf = Watir::TextField.new container, :index, 1
-    tf.method(:display_value) # should be defined
+  share_examples_for 'extended watir' do
+    it "should provide a display_value method for text fields" do
+      container = stub()
+      container.stubs(:page_container)
+      tf = @module::TextField.new container, :index, 1
+      tf.method(:display_value) # should be defined
+    end
+    
+    it "should provide a display_value method for h3's" do
+      container = stub()
+      container.stubs(:page_container)
+      h3 = @module::H3.new container, :index, 1
+      h3.method(:display_value) # should be defined
+    end
   end
-  
-  it "should provide a display_value method for h3's" do
-    container = stub()
-    container.stubs(:page_container)
-    h3 = Watir::H3.new container, :index, 1
-    h3.method(:display_value) # should be defined
+
+  describe "IE Watir" do
+    it_should_behave_like 'extended watir'
+    before do
+      @module = Watir
+    end
   end
 end
