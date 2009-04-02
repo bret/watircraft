9fd48bc3652d2b8cb32922a5d85156ba3a3ec0cb
Bret Pettichord <bret@pettichord.com>
2009-04-02 02:50:24 +0100

    use class << self instead of self.whatever

--- lib/taza/browser.rb
+++ lib/taza/browser.rb
@@ -1,46 +1,53 @@
 module Taza
   class Browser
     
-    # Create a browser instance depending on configuration.  Configuration should be read in via Taza::Settings.config.
-    # 
-    # Example:
-    #     browser = Taza::Browser.create(Taza::Settings.config)
-    #
-    def self.create(params={})
-      self.send("create_#{params[:driver]}", params)
-    end
-
-    private    
-
-    def self.create_watir(params)
-      require 'watir'
-      require 'extensions/watir'
-      Watir::Browser.default = params[:browser]
-      case params[:browser]
-        when 'ie'
-          require 'watir/ie'
-          Watir.add_display_value_methods_to Watir
-          Watir::IE.set_options(:visible => params[:visible])
-          browser = Watir::Browser.new
-          browser.speed = params[:speed]
-        when 'firefox'
-          require 'firewatir'
-          Watir.add_display_value_methods_to FireWatir
-          browser = Watir::Browser.new
-        else
-          browser = Watir::Browser.new
-      end
-          
-      browser
-    end
-
-    def self.create_selenium(params)
-      require 'selenium'
-      Selenium::SeleniumDriver.new(params[:server_ip],params[:server_port],'*' + params[:browser],params[:timeout])
-    end
+    class << self
     
-    def self.create_fake(params)
-      FakeBrowser.new
+      # Create a browser instance depending on configuration.  Configuration should be read in via Taza::Settings.config.
+      # 
+      # Example:
+      #     browser = Taza::Browser.create(Taza::Settings.config)
+      #
+      def create(params={})
+        send("create_#{params[:driver]}", params)
+      end
+  
+      private    
+  
+      def create_watir(params)
+        require 'watir'
+        require 'extensions/watir'
+        Watir::Browser.default = params[:browser]
+        case params[:browser]
+          when 'ie'
+            require 'watir/ie'
+            Watir.add_display_value_methods_to Watir
+            Watir::IE.set_options(:visible => params[:visible])
+            browser = provision_watir_browser params
+            browser.speed = params[:speed]
+          when 'firefox'
+            require 'firewatir'
+            Watir.add_display_value_methods_to FireWatir
+            browser = provision_watir_browser params
+          else
+            browser = provision_watir_browser params
+        end
+            
+        browser
+      end
+
+      def provision_watir_browser(params)
+        Watir::Browser.new
+      end
+  
+      def create_selenium(params)
+        require 'selenium'
+        Selenium::SeleniumDriver.new(params[:server_ip],params[:server_port],'*' + params[:browser],params[:timeout])
+      end
+      
+      def create_fake(params)
+        FakeBrowser.new
+      end
     end
 
   end
